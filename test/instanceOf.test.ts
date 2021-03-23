981922ad2466473007beb9ca2eb0e86cb8f7a090
skennel2 <skennel2@gmail.com>
2021-03-23 11:32:42 +0900

    source cleanup

---
+++ test/instanceOf.test.ts
@@ -0,0 +1,34 @@
+// instanceOf
+class Person implements HasName, HasNickName {
+    name: string;
+    nickName: string;
+
+    constructor(name: string, nickName: string) {
+        this.name = name;
+        this.nickName = nickName;
+    }
+}
+
+test('instanceOf 생성자로 생성한 인스턴스', () => {
+    const buddy3: Person = new Person("홍길동2", '길동이2');
+
+    expect(buddy3 instanceof Person).toEqual(true);
+});
+
+test('instanceOf Partial 타입', () => {
+    const buddy: Partial<HasName & HasNickName> = {
+        name: '홍길동',
+        nickName: '길동이'
+    };
+
+    expect(buddy instanceof Person).toEqual(false);
+});
+
+test('instanceOf & 교차타입', () => {
+    const buddy2: HasName & HasNickName = {
+        name: '홍길동',
+        nickName: '길동이'
+    };
+
+    expect(buddy2 instanceof Person).toEqual(false);
+});
--- test/intersectionType.test.ts
+++ test/intersectionType.test.ts
@@ -23,32 +23,3 @@ test('Partial', () => {
 
     expect(buddy).toHaveProperty('name', '홍길동');
 });
-
-// instanceOf
-class Person implements HasName, HasNickName {
-    name: string;
-    nickName: string;
-
-    constructor(name: string, nickName: string) {
-        this.name = name;
-        this.nickName = nickName;
-    }
-}
-
-test('instanceOf', () => {
-    const buddy: Partial<HasName & HasNickName> = {
-        name: '홍길동',
-        nickName: '길동이'
-    };
-    expect(buddy instanceof Person).toEqual(false);
-
-    const buddy2: HasName & HasNickName = {
-        name: '홍길동',
-        nickName: '길동이'
-    };
-    expect(buddy2 instanceof Person).toEqual(false);
-
-    const buddy3: Person = new Person("홍길동2", '길동이2')
-    expect(buddy3 instanceof Person).toEqual(true);
-});
-
